be6872e818b01b371a29783b8d935678877d5f40
siddiitr <siddharth.bittu@gmail.com>
2015-06-25 21:44:32 +0530

    ....

--- js/vegas.js
+++ js/vegas.js
@@ -7,10 +7,7 @@ $(function() {
 		    { src:'img/bg2.jpg', fade:1000 },
 			{ src:'img/bg3.jpg', fade:1000 },
 			{ src:'img/bg4.jpg', fade:1000 },
-			{ src:'img/bg5.JPG', fade:1000 },
-			{ src:'img/bg6.jpg', fade:1000 },
-			{ src:'img/bg7.JPG', fade:1000 },
-			{ src:'img/bg8.jpg', fade:1000 }
+			{ src:'img/bg5.JPG', fade:1000 }
 		]
 	})
 });
